153b3e3ddcb054fed4e0490e9fe849d080a2d9a4
yanzhuk <yanzhuk@gmail.com>
2021-04-22 19:18:34 +0300

    -

---
+++ src/index.js
@@ -0,0 +1,47 @@
+import './styles/app.scss';
+
+class Tabs {
+  constructor(
+    wrapperId,
+  ) {
+    this.wrapperNode = document.getElementById(wrapperId);
+    this.initTabs();
+    this.wrapperNode.addEventListener('click', (e) => this.handleWrapperClick(e));
+  }
+
+  get tabsControlsNodesArray() {
+    return Array.from(this.wrapperNode.getElementsByClassName('tabs_controls')[0].childNodes)
+      .filter(item => item.className ? item.className.includes('tab_button') : false);
+  }
+
+  get tabsContentNodesArray() {
+    return Array.from(this.wrapperNode.getElementsByClassName('tabs_content')[0].childNodes)
+      .filter(item => item.className ? item.className.includes('tab') : false);
+  }
+
+  initTabs() {
+    this.tabsControlsNodesArray[0].classList.add('active');
+    this.tabsContentNodesArray[0].classList.add('active');
+  }
+
+  setActiveTab(tabId) {
+    [
+      ...this.tabsControlsNodesArray,
+      ...this.tabsContentNodesArray
+    ].forEach(item => {
+      item.classList.remove('active')
+      if (item.getAttribute('data-table-id') === tabId) {
+        item.classList.add('active');
+      }
+    });;
+  }
+
+  handleWrapperClick({ target }) {
+    if (this.tabsControlsNodesArray.includes(target)) {
+      const tabId = target.getAttribute('data-table-id');
+      this.setActiveTab(tabId);
+    }
+  }
+}
+
+new Tabs("tabs");
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [{
+      test: /\.(css|scss|sass)$/,
+      use: [
+        'style-loader',
+        'css-loader',
+        'sass-loader'
+      ]
+    },]
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: path.resolve(__dirname, './', 'dist'),
+    compress: true,
+    hot: true,
+    port: 9000
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './', 'src/index.html'),
+    }),
+  ],
+};
